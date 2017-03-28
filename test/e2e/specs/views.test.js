7105c1b0f262301cf85d3155dc620ff029716e43
Tom Eustace <tomeustace@gmail.com>
2017-03-28 23:15:27 +0100

    Update spec to create 5 views.

--- test/e2e/specs/views.test.js
+++ test/e2e/specs/views.test.js
@@ -11,16 +11,28 @@ module.exports = {
       .clearValue('.view-name input')
       .waitForElementVisible('#tabs', 1000)
       .assert.elementPresent('.tab')
-      .pause(5000)
+      .pause(1000)
       .setValue('.view-name input', ['myview2'])
       .click('.view-save')
-      .pause(15000)
+      .pause(1000)
+      .clearValue('.view-name input')
+      .setValue('.view-name input', ['myview3'])
+      .click('.view-save')
+      .pause(1000)
+      .clearValue('.view-name input')
+      .setValue('.view-name input', ['myview4'])
+      .click('.view-save')
+      .pause(1000)
+      .clearValue('.view-name input')
+      .setValue('.view-name input', ['myview5'])
+      .click('.view-save')
+      .pause(5000)
       .elements('css selector', '.tab', function(result){
         let els = result.value;
         let count = els.length;
-        if(count !== 2) {
+        if(count !== 5) {
           //trigger failure, is there a better way to do this??
-          this.assert.value(count, 2);
+          this.assert.value(count, 5);
           this.end();
         }
         console.log('elements count is ' + count);
